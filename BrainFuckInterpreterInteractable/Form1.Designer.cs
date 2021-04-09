04fe843b5858aec27523193dc1e99581374c9957
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 12:59:11 +0100

    Add project files.

---
+++ BrainFuckInterpreterInteractable/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BrainFuckInterpreterInteractable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.input = new System.Windows.Forms.TextBox();
+            this.console = new System.Windows.Forms.ListBox();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(13, 13);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(500, 250);
+            this.input.TabIndex = 0;
+            // 
+            // console
+            // 
+            this.console.FormattingEnabled = true;
+            this.console.Location = new System.Drawing.Point(13, 326);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(500, 160);
+            this.console.TabIndex = 1;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(13, 270);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(500, 50);
+            this.start.TabIndex = 2;
+            this.start.Text = "Run";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 528);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BrainFuck Interpreter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.ListBox console;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ BrainFuckInterpreterInteractable/Form1.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrainFuckInterpreterInteractable
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string getTextFromInput()
+        {
+            return input.Text;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            console.Items.Clear();
+            console.Items.Add("");
+
+            List<int> mem = new List<int>();
+            mem.Add(0);
+
+            string file = getTextFromInput();
+            int memIndex = 0;
+            int currentLine = 0;
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                char c = file.ToCharArray()[i];
+
+                switch (c)
+                {
+                    case '>':
+                        memIndex++;
+                        if (memIndex >= mem.Count)
+                        {
+                            mem.Add(0);
+                        }
+                        break;
+                    case '<':
+                        memIndex--;
+                        if (memIndex < 0)
+                        {
+                            memIndex = 0;
+                            mem.Insert(0, 0);
+                        }
+                        break;
+                    case '+':
+                        mem[memIndex]++;
+                        if (mem[memIndex] > 255)
+                        {
+                            mem[memIndex] = 0;
+                        }
+                        break;
+                    case '-':
+                        mem[memIndex]--;
+                        if (mem[memIndex] < 0)
+                        {
+                            mem[memIndex] = 255;
+                        }
+                        break;
+                    case '.':
+                        if(mem[memIndex] == 10)
+                        {
+                            currentLine++;
+                            console.Items.Add("");
+                        } else
+                        {
+                            char letter = Convert.ToChar(mem[memIndex]);
+                            string line = (string)console.Items[currentLine];
+                            console.Items[currentLine] = (line + letter.ToString());
+                        }
+                        break;
+                    case ',':
+                        char key = Interaction.InputBox("Input").ToCharArray()[0];
+                        mem[memIndex] = (int)key;
+                        break;
+                    case '[':
+                        if (mem[memIndex] == 0)
+                        {
+                            int open = 0;
+                            i++;
+                            while (i < file.Length)
+                            {
+                                if (file[i] == ']' && open == 0)
+                                {
+                                    break;
+                                }
+                                else if (file[i] == '[')
+                                {
+                                    open++;
+                                }
+                                else if (file[i] == ']')
+                                {
+                                    open--;
+                                }
+                                i++;
+                            }
+                        }
+                        break;
+                    case ']':
+                        if (mem[memIndex] != 0)
+                        {
+                            int closed = 0;
+                            i--;
+                            while (i >= 0)
+                            {
+                                if (file[i] == '[' && closed == 0)
+                                {
+                                    break;
+                                }
+                                else if (file[i] == '[')
+                                {
+                                    closed--;
+                                }
+                                else if (file[i] == ']')
+                                {
+                                    closed++;
+                                }
+                                i--;
+                            }
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine("");
+            foreach (int num in mem)
+            {
+                Console.Write(num + ", ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Characters: " + file.Length);
+        }
+    }
+}
